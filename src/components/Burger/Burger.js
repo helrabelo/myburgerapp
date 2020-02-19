c1a5b99a356612bb12d9888e5163d585dded804b
Hel Rabelo <helrabelo@helrabelo.home>
2020-02-19 11:53:55 -0300

    adding burger ingridients and burger controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,24 @@ import classes from './Burger.css';
 import BurgerIngridient from './BurgerIngridient/BurgerIngridient';
 
 const burger = props => {
+  let transformedIngridients = Object.keys(props.ingridients)
+    .map(igKey => {
+      return [...Array(props.ingridients[igKey])].map((_, index) => {
+        return <BurgerIngridient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngridients.length === 0) {
+    transformedIngridients = <p>Please start adding ingridients</p>;
+  }
+  console.log(transformedIngridients);
   return (
     <div className={classes.Burger}>
       <BurgerIngridient type="bread-top" />
-      <BurgerIngridient type="salad" />
-      <BurgerIngridient type="cheese" />
-      <BurgerIngridient type="meat" />
+      {transformedIngridients}
       <BurgerIngridient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
+  state = {
+    ingridients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingridients={this.state.ingridients} />
+        <BuildControls />
       </Aux>
     );
   }
